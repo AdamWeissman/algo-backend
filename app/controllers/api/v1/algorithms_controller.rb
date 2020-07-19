2b4d7d55b933ce4c8c933a0f58fc06a15db9ff98
AdamWeissman <adam@comedytragedyepic.com>
2020-07-19 15:27:03 -0400

    api appears like it should work in entirety

--- app/controllers/api/v1/algorithms_controller.rb
+++ app/controllers/api/v1/algorithms_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::AlgorithmsController < ApplicationController
+
+  def index
+    @algorithms = Algorithm.all
+    render json: @algorithms
+  end
+
+  def show
+    @algorithm = Algorithm.find_by_id(params[:id])
+    render json: @algorithm
+  end
 end
--- app/controllers/api/v1/examples_controller.rb
+++ app/controllers/api/v1/examples_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::ExamplesController < ApplicationController
+
+  def index
+    @algorithm = Algorithm.find(params[:algorithm_id])
+    render json: @algorithm.examples
+  end
+
+  def create
+    @algorithm = Algorithm.find(params[:algorithm_id])
+    @example = @algorithm.example.new(example_params)
+    @example.save
+    render json: "#{@example.example} has been created"
+
+  end
+
+  def show
+    @algorithm = Algorithm.find(params[:algorithm_id])
+    render json: "SOME MESSAGE: #{@algorithm.algotype}"
+  end
+
+  private
+
+  def example_params
+    params.require(:example).permit(:example) 
+  end
+
+
 end
