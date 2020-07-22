ceae8b0bf35078dd918a85144d365fd3fd29437a
AdamWeissman <adam@comedytragedyepic.com>
2020-07-22 17:16:44 -0400

    render algos with examples only route created and tested

--- app/controllers/api/v1/algorithms_controller.rb
+++ app/controllers/api/v1/algorithms_controller.rb
@@ -1,12 +1,26 @@
 class Api::V1::AlgorithmsController < ApplicationController
 
+  def idxe
+    @algorithms = Algorithm.all
+    @algos = []
+    @algorithms.each do |algo|
+      if algo.examples.length >= 1
+        @algos << algo
+      else
+        next
+      end
+    end
+      
+    render json: @algos
+  end
+
   def index
     @algorithms = Algorithm.all
     render json: @algorithms
   end
 
-  def show
-    @algorithm = Algorithm.find_by_id(params[:id])
-    render json: @algorithm.examples
-  end
+  # def show
+  #   @algorithm = Algorithm.find_by_id(params[:id])
+  #   render json: @algorithm.examples
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :algorithms, only: [:index, :show] do
+      resources :algorithms, only: [:index] do
         resources :examples, only: [:create, :show, :index]
       end
     end
   end
+
+  get "/api/v1/algorithms/idxe" => 'api/v1/algorithms#idxe'
 
 end
