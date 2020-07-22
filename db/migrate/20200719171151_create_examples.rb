a52f391862599dfdc43225d31f3df22d00452230
AdamWeissman <adam@comedytragedyepic.com>
2020-07-22 01:25:06 -0400

    adding title to the example migration file so anonymous users can name their examples

--- db/migrate/20200719171151_create_examples.rb
+++ db/migrate/20200719171151_create_examples.rb
@@ -1,6 +1,7 @@
 class CreateExamples < ActiveRecord::Migration[6.0]
   def change
     create_table :examples do |t|
+      t.string :title
       t.text :example
       t.belongs_to :algorithm, null: false, foreign_key: true
     
