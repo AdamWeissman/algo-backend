7d7b23659e1f213dc29c5d48c06d18e22dd63e52
AdamWeissman <adam@comedytragedyepic.com>
2020-08-10 16:46:14 -0400

    set up backend server to delete examples/koans that are more than 2 days old

--- app/controllers/api/v1/examples_controller.rb
+++ app/controllers/api/v1/examples_controller.rb
@@ -2,8 +2,13 @@ class Api::V1::ExamplesController < ApplicationController
 
   def index
     @algorithm = Algorithm.find(params[:algorithm_id])
-    render json: @algorithm.examples
+    @allExamples = @algorithm.examples
+    rightNow = DateTime.now.to_f
+    @zenExamples = @allExamples.map { |example| ((rightNow - example.created_at.to_f) < 172800.0 ) ? example : example.delete } 
+    render json: @zenExamples.compact #@algorithm.examples
   end
+
+  # note 172800.0 is 60 * 60 * 24 * 2 ... or 2 days.  Basically, the above check is to make sure that the example was created within 2 days
 
   def create
     @algorithm = Algorithm.find(params[:algorithm_id])
@@ -12,13 +17,13 @@ class Api::V1::ExamplesController < ApplicationController
     render json: @example.id
   end
 
-  def update
-    @algorithm = Algorithm.find(params[:algorithm_id])
-    @example = @algorithm.examples.find(params[:id])
-    @example.content = params["content"]
-    @example.save
-    render json: "#{@example.title} has been updated with #{@example.content}"
-  end
+  # def update
+  #   @algorithm = Algorithm.find(params[:algorithm_id])
+  #   @example = @algorithm.examples.find(params[:id])
+  #   @example.content = params["content"]
+  #   @example.save
+  #   render json: "#{@example.title} has been updated with #{@example.content}"
+  # end
 
 
   def show
